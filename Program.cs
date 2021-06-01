3c723e15411077f39b91ee3e5fd19eb96ff33559
Russell Jewett <rfjewett3@gmail.com>
2021-06-01 16:27:00 -0400

    updates the logic for placement of employee photo on badge graphic.

--- Program.cs
+++ Program.cs
@@ -75,9 +75,9 @@ namespace CatWorx.BadgeMaker
                 Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
                 Graphics graphic = Graphics.FromImage(badge);
                 graphic.DrawImage(background, new Rectangle(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+                graphic.DrawImage(photo, new Rectangle(PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+                badge.Save("data/employeeBadge.png");
                 // background.Save("data/employeeBadge.png");
-                Console.WriteLine("Photo: " + photo.GetType());
-                Console.WriteLine("Background: " + background.GetType());
             }
         }
     }
